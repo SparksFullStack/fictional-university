3302b298fb884300b42a9829a182447fd2007006
SparksFullStack <csparks@antioch.edu>
2019-03-05 18:44:25 -0800

    blog section added--pagination working

--- index.php
+++ index.php
@@ -15,18 +15,19 @@
         while(have_posts()) {
             the_post(); ?>
             <div class="post-item">
-                <h2><a href="<?=the_permalink()?>"><?=the_title()?></a></h2>
+                <h2 class="headline headline--medium headline--post-title"><a href="<?=the_permalink()?>"><?=the_title()?></a></h2>
 
                 <div class="metabox">
-                    <p>Posted by Charlie in News</p>
+                    <p>Posted by <?=the_author_posts_link()?> on <?=the_time("n.j.y")?> in <?=get_the_category_list(', ')?></p>
                 </div>
 
                 <div class="generic-content">
                     <?=the_excerpt()?>
-                    <p><a href="<?=the_permalink()?>">Continue Reading &raquo;</a></p>
+                    <p><a class="btn btn--blue" href="<?=the_permalink()?>">Continue Reading &raquo;</a></p>
                 </div>
             </div>
         <?php }
+        echo paginate_links();
     ?>
 </div>
 
