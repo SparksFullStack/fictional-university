17d51b956507cf5ae9bf869e77fc71fb03b80bb0
SparksFullStack <csparks@antioch.edu>
2019-03-05 16:32:49 -0800

    added dynamic linking and text for the breadcrumbs

--- page.php
+++ page.php
@@ -16,10 +16,11 @@
         <div class="container container--narrow page-section">
 
         <?php
-            if (wp_get_post_parent_id(get_the_ID())){
+            $the_parent = wp_get_post_parent_id(get_the_ID());
+            if ($the_parent){
                ?>
                 <div class="metabox metabox--position-up metabox--with-home-link">
-                    <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?=the_title()?></span></p>
+                    <p><a class="metabox__blog-home-link" href="<?=get_permalink($the_parent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?=get_the_title($the_parent)?></a> <span class="metabox__main"><?=the_title()?></span></p>
                 </div>
                 <?php
             } 
