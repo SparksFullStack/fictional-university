450c10e2da21846810c7de029802001ae6074dff
SparksFullStack <csparks@antioch.edu>
2019-03-06 12:53:27 -0800

    custom archive for events setup

--- single-event.php
+++ single-event.php
@@ -1,1 +1,28 @@
-test123
+<?php get_header();?>
+
+<?php
+    the_post();
+    ?> 
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?=get_theme_file_uri('images/ocean.jpg');?>);"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?=the_title();?></h1>
+                <div class="page-banner__intro">
+                    <p>REPLACE LATER</p>
+                </div>
+            </div>  
+        </div>
+        
+        <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?=site_url('/events')?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span class="metabox__main"><?=the_title()?></span></p>
+            </div>
+
+            <div class="generic-content">
+                <?=the_content()?>
+            </div>
+        </div>
+    <?php
+?>
+
+<?php get_footer();?>
