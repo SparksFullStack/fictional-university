1d39369aa38b66339adedecdeb924d262985c3c1
SparksFullStack <csparks@antioch.edu>
2019-03-05 16:57:41 -0800

    loading the breadcrumbs menu conditionally only if the page is a parent or has a parent

--- page.php
+++ page.php
@@ -17,6 +17,8 @@
 
         <?php
             $the_parent = wp_get_post_parent_id(get_the_ID());
+            $the_child = "";
+
             if ($the_parent){
                ?>
                 <div class="metabox metabox--position-up metabox--with-home-link">
@@ -24,16 +26,35 @@
                 </div>
                 <?php
             } 
-        ?>
-            
-            
-            <!-- <div class="page-links">
-                <h2 class="page-links__title"><a href="#">About Us</a></h2>
-                <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
-                </ul>
-            </div> -->
+
+            ?>
+
+                <?php
+                    // this will return the collection of child elements if they exist for the current page.
+                    $child_collection = get_pages(array( 'child_of' => get_the_ID()));
+
+                    // here the menu will only be loaded if the page has a parent page, or has a collection of child pages
+                    if ($the_parent or $child_collection) { ?>
+                        <div class="page-links">
+                            <!-- Below, if there is a parent value, it will get the title of the parent; otherwise it will get the current title -->
+                            <h2 class="page-links__title"><a href="<?= get_permalink($the_parent)?>"><?=get_the_title($the_parent)?> </a></h2> 
+                            <ul class="min-list">
+                                <?php
+                                    if ($the_parent){
+                                        $child_of_value = $the_parent; // if there is a parent element, we want its ID
+                                    } else {
+                                        $child_of_value = get_the_ID(); // if there is no parent, we want the current ID
+                                    }
+
+                                    wp_list_pages(array(
+                                        'title_li' => NULL,
+                                        'child_of' => $child_of_value
+                                    ))
+                                ?>
+                            </ul>
+                        </div>
+                   <?php }
+                ?>
 
             <div class="generic-content">
                 <?=the_content();?>
